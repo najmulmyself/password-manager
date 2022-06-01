87a8d6413481dc73e42722bcbd6c08aa71b6047b
najmulmyself <najmul.myself@gmail.com>
2022-06-02 01:17:09 +0600

    password entry and generate btn is not aligned yet

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=logo_img)
 canvas.grid(row=0,column=1)
 
+#Label
 
 website_label = Label(text='Website')
 website_label.grid(row=1,column=0)
 
-email_label = Label(text='Email/Username')
+email_label = Label(text='Email')
 email_label.grid(row=2,column=0)
 
 password_label = Label(text='Password')
 password_label.grid(row=3,column=0)
 
+
+#Entry
 website_entry = Entry(width=35)
 website_entry.grid(row=1,column=1,columnspan=2)
 
@@ -30,9 +33,13 @@ email_entry.grid(row=2,column=1,columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=3,column=1)
 
-button = Entry(width=14)
-button = Button(text='generate')
-button.grid(row=3,column=2)
+#Button
+
+generate_password_btn = Button(text='Generate Password')
+generate_password_btn.grid(row=3,column=2)
+
+add_button = Button(text="Add",width=36)
+add_button.grid(row=4,column=1,columnspan=2)
 
 
 window.mainloop()
