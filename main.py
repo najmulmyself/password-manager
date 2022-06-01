9ade00509e527dd720ab02d5979d58c3c3d1c2f2
najmulmyself <najmul.myself@gmail.com>
2022-06-01 22:22:44 +0600

    need to resize image in canvas

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from email.mime import image
 from tkinter import *
 
 window = Tk()
 window.title('Password Manager')
 
 canvas = Canvas(height=200,width=200)
+logo_img = PhotoImage(file="logo.png")
+# logo = logo_img.resize((50,50),Image.ANTIALIAS)
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
 
 
 window.mainloop()
