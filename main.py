03e4048739a79d81b673cd5c3a745dd3ceba77f4
najmulmyself <najmul.myself@gmail.com>
2022-06-02 23:14:18 +0600

    inserted field

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ website_entry.focus
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,'najmulmyself@gmail.com')
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3,column=1)
