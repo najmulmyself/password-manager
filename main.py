e3e38af3c4a1e19d2eaff26455558f335190c021
najmulmyself <najmul.myself@gmail.com>
2022-06-05 22:53:36 +0600

    password generator

--- main.py
+++ main.py
@@ -34,6 +34,22 @@ def get_entry():
                 password_entry.delete(0,END)
 
 
+
+# Password Generator
+
+letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+             'Y', 'Z']
+number = [1,2,3,4,5,6,7,8,9]
+symbol = [' ', '!', '@', '#', '$', '%', '^', '&',
+             '*', '(', ')']
+
+
+
+
 #Label
 
 website_label = Label(text='Website')
